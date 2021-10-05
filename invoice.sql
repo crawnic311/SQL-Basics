f7ccc9feb9279b7d80e169a6db827fac09b10721
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-05 15:53:08 -0600

    Problem 1 Invoice

---
+++ invoice.sql
@@ -0,0 +1,44 @@
+--Problem 1
+--Count how many orders were made from the USA.
+SELECT COUNT(*) FROM invoice
+WHERE billing_country = 'USA'
+
+
+
+--Problem 2
+--Find the largest order total amount.
+
+
+--Problem 3
+--Find the smallest order total amount.
+
+
+--Problem 4
+--Find all orders bigger than $5.
+
+
+--Problem 5
+--Count how many orders were smaller than $5.
+
+
+
+--Problem 6
+--Count how many orders were in CA, TX, or AZ (use IN).
+
+
+--Problem 7
+--Get the average total of the orders.
+
+
+--Problem 8
+--Get the total sum of the orders.
+
+
+--Problem 9
+--Update the invoice with an invoice_id of 5 to have a total order amount of 24.
+
+
+
+--Problem 10
+--Delete the invoice with an invoice_id of 1.
+
