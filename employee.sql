15f0c85e3e2c70a879c66837abc881b0c2725ff3
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-05 15:40:06 -0600

    Problem 1 Employee

---
+++ employee.sql
@@ -0,0 +1,30 @@
+--Problem 1
+--List all employee first and last names only that live in Calgary.
+SELECT first_name, last_name FROM employee 
+WHERE city = 'Calgary'
+
+
+
+--Problem 2
+--Find the birthdate for the youngest employee.
+
+
+
+
+
+--Problem 3
+--Find the birthdate for the oldest employee.
+
+
+
+
+
+--Problem 4
+--Find everyone that reports to Nancy Edwards (use the ReportsTo column). * You will need to query the employee table to find the id for Nancy Edwards
+
+
+
+
+
+--Problem 5
+--Count how many people live in Lethbridge.
