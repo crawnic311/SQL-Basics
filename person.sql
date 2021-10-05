03b8f3dab40c90e0a549208bca6608ae67f204fb
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-05 13:42:14 -0600

    Problem 1

---
+++ person.sql
@@ -0,0 +1,62 @@
+--Problem 1
+--Create a table called person that records a person’s id, name, age, height (in cm , city, favorite_color. id should be an auto-incrementing id/primary key (use type: SERIAL)
+CREATE TABLE person(
+  persons_id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  age INTEGER,
+  height INTEGER,
+  city VARCHAR(255),
+  favorite_color VARCHAR(255)
+);
+
+
+--Problem 2
+--Add 5 different people into the person database. Remember to not include the person_id because it should auto-increment.
+
+
+
+--Select all the people in the person table by height from tallest to shortest.
+
+
+
+--Select all the people in the person table by height from shortest to tallest.
+
+
+
+--Select all the people in the person table by age from oldest to youngest.
+
+
+
+--Select all the people in the person table older than age 20.
+
+
+
+--Select all the people in the person table that are exactly 18.
+
+
+
+--Select all the people in the person table that are less than 20 and older than 30.
+
+
+
+--Select all the people in the person table that are not 27 (use not equals).
+
+
+
+--Select all the people in the person table where their favorite color is not red.
+
+
+
+--Select all the people in the person table where their favorite color is not red and is not blue.
+
+
+
+--Select all the people in the person table where their favorite color is orange or green.
+
+
+
+--Select all the people in the person table where their favorite color is orange, green or blue (use IN).
+
+
+
+--Select all the people in the person table where their favorite color is yellow or purple (use IN).//
