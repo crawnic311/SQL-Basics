2a69aa70361a8aabcaefc366d88656998c2e3c81
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-05 15:28:28 -0600

    Problem 1 Artist

---
+++ artist.sql
@@ -0,0 +1,30 @@
+--Problem 1
+--Add 3 new artists to the artist table. (It already exists.)
+INSERT INTO artist (name)
+VALUES
+	('311'),
+  ('Radiohead'),
+  ('Ruston Kelly');
+
+
+
+--Problem 2
+--Select 10 artists in reverse alphabetical order.
+
+
+
+
+--Problem 3
+--Select 5 artists in alphabetical order.
+
+
+
+
+--Problem 4
+--Select all artists that start with the word ‘Black’.
+
+
+
+
+--Problem 5
+--Select all artists that contain the word ‘Black’.
