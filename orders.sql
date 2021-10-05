f845161700d6beec2611481586b14b21909317ff
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-05 14:56:30 -0600

    Problem 1 Orders

---
+++ orders.sql
@@ -0,0 +1,37 @@
+--Problem 1
+--Create a table called orders that records: order_id, person_id, product_name, product_price, quantity.
+CREATE TABLE orders(
+  order_id INTEGER,
+  person_id INTEGER,
+  product_name VARCHAR(255),
+  product_price FLOAT,
+  quantity INTEGER
+);
+
+
+--Problem 2
+--Add 5 orders to the orders table.
+-----Make orders for at least two different people.
+-----person_id should be different for different people.
+
+
+
+
+--Problem 3
+--Select all the records from the orders table.
+
+
+
+
+--Problem 4
+--Calculate the total number of products ordered.
+
+
+
+--Problem 5
+--Calculate the total order price.
+
+
+
+--Problem 6
+--Calculate the total order price by a single person_id.
