fbcd47d93e4d10aadfa5e6fc7a3fae5588eac0cd
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-05 16:03:14 -0600

    Problem 9

--- invoice.sql
+++ invoice.sql
@@ -44,7 +44,9 @@ SELECT SUM(TOTAL) FROM invoice
 
 --Problem 9
 --Update the invoice with an invoice_id of 5 to have a total order amount of 24.
-
+UPDATE invoice 
+SET total = 24
+WHERE invoice_id = 5
 
 
 --Problem 10
